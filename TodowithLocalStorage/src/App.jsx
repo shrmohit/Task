3b133da75833b74e240eec94edd139c35673a71c
mohit sharma <mohitsharma7258@gmail.com>
2025-07-23 11:33:46 +0530

    todo with delete and update

--- TodowithLocalStorage/src/App.jsx
+++ TodowithLocalStorage/src/App.jsx
@@ -1,16 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState({
+    usertask: "",
+    date: ""
+  })
+  const [editIndex, setEditIndex] = useState(null)
+  const [taskList, setTaskList] = useState([])
+  const handleInput = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value })
+  }
+
+  const handleFormData = (e) => {
+    e.preventDefault()
+    console.log(input);
+    setTaskList([...taskList, input])
+    console.log(taskList);
+
+    if (editIndex !== null) {
+      const updateTask = [...taskList]
+      updateTask[editIndex] = input
+      setTaskList(updateTask)
+      setEditIndex(null)
+    }
+    setInput({ usertask: "", date: "" })
+
+  }
+
+  const handleEdit = (task, index) => {
+    setInput(task)
+    setEditIndex(index)
+  }
+
+  const handleDelete = (index) => {
+    let update = taskList.filter((task, i) => i !== index)
+    setTaskList(update)
+  }
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div>
+        <form onSubmit={handleFormData} >
+          <label>Task</label>
+          <input type="text"
+            name='usertask'
+            value={input.usertask}
+            onChange={handleInput}
+            placeholder='Enter Task' />
+          <label >Date</label>
+          <input type="date"
+            name='date'
+            value={input.date}
+            onChange={handleInput}
+            placeholder='Enter Date' />
+          <button type='submit'>Submit</button>
+        </form>
+
+        <div>
+          {
+            taskList?.map((data, index) => (
+              <div className='flex justify-center flex-row  space-x-6' key={index}>
+                <p>{data.usertask}</p>
+                <p>{data.date}</p>
+                <button onClick={() => handleDelete(index)}>Delete</button>
+                <button onClick={() => handleEdit(data, index)}>Edit</button>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </>
   )
 }
